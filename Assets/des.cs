f2ba6d0b923048a22518f39089e90e3141bed74c
Superion13 <Justin.jrs13@yahoo.com>
2016-04-08 17:55:57 -0500

    210 project

---
+++ Assets/des.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class des : MonoBehaviour{
+	public GameObject explosion;
+	
+	void OnTriggerExit(Collider Other)
+	{
+		if (Other.tag == "w") {
+			return;
+		}
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy(Other.gameObject);
+		Destroy (gameObject);
+	}
+}
